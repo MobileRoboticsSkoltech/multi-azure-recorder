e0e92bb0f76f9c51e03c75c02be99defd3b8125c
Marselka <ma.faizullin@gmail.com>
2022-02-25 09:30:26 +0300

    exchange the timestamps and imu data extraction tools

--- extractor.sh
+++ extractor.sh
@@ -2,6 +2,8 @@
 
 #Usage: bash extractor input_directory output_directory
 
+#This block is to get exact path of the current script since internal structure of the repo path assumed to be constant
+#and Azure tools are inside
 SOURCE=${BASH_SOURCE[0]}
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
   DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
@@ -9,6 +11,9 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
+TIMESTAMPS_EXTRACTOR_EXE="Azure-Kinect-Sensor-SDK/build/bin/mrob_timestamps_extractor"
+IMU_DATA_EXTRACTOR_EXE="Azure-Kinect-Sensor-SDK/build/bin/mrob_imu_data_extractor"
 
 input_path="$1"
 output_path="$DIR/output/$(basename -- $1)"
@@ -20,21 +25,18 @@ for mkv_path in $(find $input_path -name '*.mkv' | sed 's,.*/,,' | sed  's,.mkv,
 
 	mkdir -p $output_path/$mkv_path/{color,depth}
 
-	cp $(find $input_path -name '*.json') $output_path/$(find $input_path -name '*.json' | sed 's,.*/,,')
-	mv $output_path/$(find $input_path -name '*.json' | sed 's,.*/,,') $output_path/recording_params.json
-
 	ffmpeg -i $input_path/$mkv_path'.mkv' -map 0:0 -vsync 0 $output_path/$mkv_path/color/%d.png
 	ffmpeg -i $input_path/$mkv_path'.mkv' -map 0:1 -vsync 0 $output_path/$mkv_path/depth/%d.png
 
-	mkvextract $input_path/$mkv_path'.mkv' timestamps_v2 3:$output_path/$mkv_path/imu.csv
-	mkvextract $input_path/$mkv_path'.mkv' timestamps_v2 0:$output_path/$mkv_path/color_timestamps.csv 1:$output_path/$mkv_path/depth_timestamps.csv
-
+	$TIMESTAMPS_EXTRACTOR_EXE $input_path/$mkv_path'.mkv' $output_path/$mkv_path
+	$IMU_DATA_EXTRACTOR_EXE $input_path/$mkv_path'.mkv' $output_path/$mkv_path/imu.csv
 
 	sed -i '1d' $output_path/$mkv_path/color_timestamps.csv
 	N=1
 	while read new_name;
 		do
-		mv $output_path/$mkv_path'/color/'$N'.png' $output_path/$mkv_path'/color/'$new_name'.png'
+		new_name_formatted=$(printf "%012d" $new_name)
+		mv $output_path/$mkv_path'/color/'$N'.png' $output_path/$mkv_path'/color/'$new_name_formatted'.png'
 		((N++))   
 		done < $output_path/$mkv_path/color_timestamps.csv;
 
@@ -43,9 +45,10 @@ for mkv_path in $(find $input_path -name '*.mkv' | sed 's,.*/,,' | sed  's,.mkv,
 	N=1
 	while read new_name
 		do
-		mv $output_path/$mkv_path'/depth/'$N'.png' $output_path/$mkv_path'/depth/'$new_name'.png'
+		new_name_formatted=$(printf "%012d" $new_name)
+		mv $output_path/$mkv_path'/depth/'$N'.png' $output_path/$mkv_path'/depth/'$new_name_formatted'.png'
 		((N++))   
 		done < $output_path/$mkv_path/depth_timestamps.csv;
 
-	rm $output_path/$mkv_path/color_timestamps.csv $output_path/$mkv_path/depth_timestamps.csv
+	#rm $output_path/$mkv_path/color_timestamps.csv $output_path/$mkv_path/depth_timestamps.csv
 done
