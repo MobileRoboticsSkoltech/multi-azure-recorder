97005286d39114ebbda3c1fe294bff6122ba37d5
Marselka <ma.faizullin@gmail.com>
2022-03-15 14:45:37 +0300

    extend extractor by cameras calib params extractor

--- extractor.sh
+++ extractor.sh
@@ -14,9 +14,10 @@ DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
 
 TIMESTAMPS_EXTRACTOR_EXE="Azure-Kinect-Sensor-SDK/build/bin/mrob_timestamps_extractor"
 IMU_DATA_EXTRACTOR_EXE="Azure-Kinect-Sensor-SDK/build/bin/mrob_imu_data_extractor"
+CALIB_PARAMS_EXTRACTOR_EXE="Azure-Kinect-Sensor-SDK/build/bin/mrob_calibration_params_extractor"
 
 input_path="$1"
-output_path="$DIR/output/$(basename -- $1)"
+output_path="$DIR/extracted-data/$(basename -- $1)"
 
 rm -rf $output_path
 
@@ -27,10 +28,11 @@ for mkv_path in $(find $input_path -name '*.mkv' | sed 's,.*/,,' | sed  's,.mkv,
 
 	ffmpeg -i $input_path/$mkv_path'.mkv' -map 0:0 -vsync 0 $output_path/$mkv_path/color/%d.png
 	ffmpeg -i $input_path/$mkv_path'.mkv' -map 0:1 -vsync 0 $output_path/$mkv_path/depth/%d.png
+	#ffmpeg -i $input_path/$mkv_path'.mkv' -map 0:2 -vsync 0 $output_path/$mkv_path/ir/%d.png
 
-	$TIMESTAMPS_EXTRACTOR_EXE $input_path/$mkv_path'.mkv' $output_path/$mkv_path
-	$IMU_DATA_EXTRACTOR_EXE $input_path/$mkv_path'.mkv' $output_path/$mkv_path/imu.csv
-
+	$TIMESTAMPS_EXTRACTOR_EXE   $input_path/$mkv_path'.mkv' $output_path/$mkv_path
+	$IMU_DATA_EXTRACTOR_EXE     $input_path/$mkv_path'.mkv' $output_path/$mkv_path/imu.csv
+	$CALIB_PARAMS_EXTRACTOR_EXE $input_path/$mkv_path'.mkv' $output_path/$mkv_path/calib_params.json
 	sed -i '1d' $output_path/$mkv_path/color_timestamps.csv
 	N=1
 	while read new_name;
